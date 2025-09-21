5c4b534329981f8490dffab212be241aeaf61337
Zee <100657535+zxzeexz@users.noreply.github.com>
2025-09-21 21:10:23 +0800

    Rev 2 for version 1.5

--- lib/session.php
+++ lib/session.php
@@ -2,7 +2,7 @@
 /**
  * lib/session.php
  * ZPanel session handler
- * Revision 1 [9-12-2025]
+ * Revision 2 [9-21-2025]
  * Zee ^_~
  */
 class Session
@@ -16,9 +16,14 @@ class Session
     // Create a new session in cp_sessions
     public static function create(int $accountId, string $username): bool
     {
+		// Cleanup expired sessions before creating a new one
+		$sqlCleanup = "DELETE FROM cp_sessions WHERE expires_at < :now";
+		db_execute($sqlCleanup, [':now' => time()]);
+		
         $sessionId = bin2hex(random_bytes(32));
         $ip        = getUserIp();
-        $expiry    = time() + (60 * 60); // 1 hour default
+		$lifetime  = $config['security']['max_logintime'] ?? 3600;
+        $expiry    = time() + $lifetime;
 
         // Save in DB
         $sql = "INSERT INTO cp_sessions (session_id, account_id, ip_address, expires_at) 
@@ -47,7 +52,10 @@ class Session
         if (empty($sessionId)) {
             return false;
         }
-
+		// Cleanup: remove expired sessions
+		$sqlCleanup = "DELETE FROM cp_sessions WHERE expires_at < :now";
+		db_execute($sqlCleanup, [':now' => time()]);
+		
         $sql  = "SELECT * FROM cp_sessions WHERE session_id = :sid LIMIT 1";
         $row  = db_fetch($sql, [':sid' => $sessionId]);
 
